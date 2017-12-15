ddd79d740c6a3ad1917d6f8beee07768921809f4
Christine Banek <cbanek@cbanekpc.lsst.local>
2017-12-15 15:11:06 -0700

    Initial commit of astropixie library!

---
+++ astropixie/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='astropixie',
+      version='0.0.1',
+      description='EPO data client',
+      url='https://github.com/lsst-epo/epoc',
+      author='Christine Banek',
+      author_email='cbanek@lsst.org',
+      license='MIT',
+      packages=['astropixie'],
+      install_requires=['pytest'])
