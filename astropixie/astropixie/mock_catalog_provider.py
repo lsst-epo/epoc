e8d63e0c8306dd24f99462651411447aecb37854
Christine Banek <cbanek@lsst.org>
2017-12-22 15:22:31 -0700

    [EPO-359] Initial astropixie implementation
    This commit makes a mock implementation for the astropixie library,
    which gets a lot of the plumbing together.  This includes a mock
    data provider which reads from files on disk to simulate having a
    data center backing us.

---
+++ astropixie/astropixie/mock_catalog_provider.py
@@ -0,0 +1,43 @@
+from astropy.coordinates import SkyCoord
+import os.path
+import numpy
+
+from astropixie.types import OpenCluster, Star
+
+
+class MockCatalogProvider:
+    def __init__(self):
+        self.clusters = {
+            OpenCluster('Berkeley 20', SkyCoord(ra='5h33m0s', dec='0h13m0s')):
+                self.list_berkeley20,
+        }
+
+    def list_open_clusters(self):
+        return list(self.clusters.keys())
+
+    def list_stars(self, coord):
+        for cluster, f in self.clusters.items():
+            if cluster.coord == coord:
+                return f()
+
+        return []
+
+    def list_berkeley20(self):
+        datafile = self._get_mock_data_path('berkeley20.tsv')
+        stars = numpy.genfromtxt(
+            datafile, delimiter=';', skip_header=41,
+            names=['Seq', 'Xpos', 'Ypos', 'Vmag', 'V_R', 'e_Vmag', 'e_Rmag']
+        )
+
+        results = []
+
+        for star in stars:
+            v_mag = star['Vmag']
+            r_mag = (star['V_R'] * -1.0) + v_mag
+            results.append(Star(v_mag, r_mag))
+
+        return results
+
+    def _get_mock_data_path(self, name):
+        modPath = os.path.dirname(__file__)
+        return os.path.join(modPath, 'mock_data', name)
---
+++ astropixie/astropixie/types.py
@@ -0,0 +1,16 @@
+class OpenCluster:
+    def __init__(self, friendlyName, skyCoords):
+        self.name = friendlyName
+        self.coord = skyCoords
+
+    def __repr__(self):
+        return '<astropixie.OpenCluster %s %s>' % (self.name, self.coord)
+
+
+class Star:
+    def __init__(self, Vmag, Rmag):
+        self.Vmag = Vmag
+        self.Rmag = Rmag
+
+    def __repr__(self):
+        return '<astropixie.Star V=%f R=%f>' % (self.Vmag, self.Rmag)
