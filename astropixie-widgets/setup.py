cfc50d7348d4f10e350711a70b49044ac98379ce
J. Matt Peterson <jmatt@jmatt.org>
2018-04-04 14:18:44 -0700

    Rename `astropixie-hr` to `astropixie-widgets`.
    	new file:   astropixie-widgets/.gitignore
    	renamed:    astropixie-hr/AUTHORS -> astropixie-widgets/AUTHORS
    	new file:   astropixie-widgets/README.md
    	renamed:    astropixie-hr/astropixie_hr/__init__.py -> astropixie-widgets/astropixie_widgets/__init__.py
    	renamed:    astropixie-hr/astropixie_hr/config.py -> astropixie-widgets/astropixie_widgets/config.py
    	renamed:    astropixie-hr/astropixie_hr/science.py -> astropixie-widgets/astropixie_widgets/science.py
    	renamed:    astropixie-hr/astropixie_hr/visual.py -> astropixie-widgets/astropixie_widgets/visual.py
    	renamed:    astropixie-hr/setup.py -> astropixie-widgets/setup.py
    	new file:   astropixie/.gitignore

--- astropixie-widgets/setup.py
+++ astropixie-widgets/setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup
 
-setup(name='astropixie-hr',
+setup(name='astropixie-widgets',
       version='0.1.0',
-      description='LSST EPO Hertzsprung-Russell (HR) Diagram library.',
+      description='LSST EPO ',
       url='https://github.com/lsst-epo/vela/astropixie-hr',
       author='LSST EPO Team',
       author_email='epo-team@lists.lsst.org',
       license='MIT',
-      packages=['astropixie_hr'],
+      packages=['astropixie_widgets'],
       include_package_data=True,
       package_data={'astropixie': ['sample_data/*']},
       install_requires=[
           'astropixie',
-          'astropy',
+          'astropy==3.0',
           'astroquery',
-          'bokeh',
+          'bokeh>=0.12.15',
           # 'ipyaladin',
-          'numpy',
+          'numpy==1.14.1',
           'pandas',
           'pytest',
           'scipy'
