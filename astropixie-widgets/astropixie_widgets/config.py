2e8df85dc4ee3e72449b7b115a36449778313fe0
Christine Banek <cbanek@lsst.org>
2018-05-30 13:49:26 -0700

    [EPO-789] Get rid of extra copy of jupyter_proxy_url
    This was really complicated, there's additional copies of the code,
    and use of global in here makes it even more complicated as to what
    is going on.
    
    Also, get rid of local mode in setup_notebook.  Detect this by the
    existance of the environment variable JUPYTERHUB_SERVICE_PREFIX.

--- astropixie-widgets/astropixie_widgets/config.py
+++ astropixie-widgets/astropixie_widgets/config.py
@@ -30,7 +30,7 @@ def remote_jupyter_proxy_url(port):
 jupyter_proxy_url = remote_jupyter_proxy_url
 
 
-def setup_notebook(debug=False, local=False):
+def setup_notebook(debug=False):
     """Called at the start of notebook execution to setup the environment.
 
     This will configure bokeh, and setup the logging library to be
@@ -41,26 +41,12 @@ def setup_notebook(debug=False, local=False):
         logging.basicConfig(level=logging.DEBUG)
         logging.debug('Running notebook in debug mode.')
 
-    if local:
-        global jupyter_proxy_url
+    # If JUPYTERHUB_SERVICE_PREFIX environment variable isn't set,
+    # this means that you're running JupyterHub not with Hub in k8s,
+    # and not using run_local.sh (which sets it to empty).
+    if 'JUPYTERHUB_SERVICE_PREFIX' not in os.environ:
         jupyter_proxy_url = 'localhost:8888'
-    else:
-        jupyter_proxy_url = remote_jupyter_proxy_url
 
 
 def show_with_bokeh_server(obj):
-    def jupyter_proxy_url(port):
-        # If port is None we're asking about the URL
-        # for the origin header.
-        if port is None:
-            return '*'
-
-        base_url = os.environ['EXTERNAL_URL']
-        service_url_path = os.environ['JUPYTERHUB_SERVICE_PREFIX']
-        proxy_url_path = 'proxy/%d' % port
-
-        user_url = urllib.parse.urljoin(base_url, service_url_path)
-        full_url = urllib.parse.urljoin(user_url, proxy_url_path)
-        return full_url
-
     show(obj, notebook_url=jupyter_proxy_url)
