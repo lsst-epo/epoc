c7e942b0f221cec8bf059df8582a612aebb03d27
Christine Banek <cbanek@lsst.org>
2018-06-26 16:12:53 -0700

    [EPO-878] Additional fixes to logger to hide bokeh messages.
    Hide bokeh messages by setting their logger up appropriately (they
    are trying to be cute in their code and overriding the default
    behavior).
    
    Also get rid of the pattern about passing around the logger.  Just
    use the logging library convenience functions instead, and because
    the format string is great, that should be more than enough.

--- astropixie-widgets/astropixie_widgets/config.py
+++ astropixie-widgets/astropixie_widgets/config.py
@@ -31,32 +31,39 @@ def remote_jupyter_proxy_url(port):
 jupyter_proxy_url = remote_jupyter_proxy_url
 
 
-logger = None
-
-
 def _setup_logging(level):
     format_ = "%(asctime)s %(name)s-%(levelname)s "\
               "[%(pathname)s %(lineno)d] %(message)s"
-    formatter = logging.Formatter(format_)
-    logging.basicConfig(level=level, format=format_)
-    global logger
-    logger = logging.getLogger()
-    logger.handlers.clear()
-    
+
+    # Just in case basicConfig hasn't been called yet...
+    # although many libraries will call it for us upon loading,
+    # like bokeh.  But if none of them do, and basicConfig hasn't
+    # been called before setup_notebook, call it here.
+    # If basicConfig has already been called, this is ignored.
+    logging.basicConfig(format=format_)
+
+    # Set up the file, filehandler, and the formatter.
     filename = Path('~/ipynb.log').expanduser()
     filename.touch()
 
     handler = logging.FileHandler(filename=filename,
                                   encoding='utf-8', mode='a+')
+    formatter = logging.Formatter(format_)
     handler.setFormatter(formatter)
-    handler.setLevel(level)
 
-    logger.setLevel(level)
+    # Get the root logger, clear our uses of STDERR, and add our handler,
+    # and set the root logger to the appropriate level.
+    logger = logging.getLogger()
+    logger.handlers.clear()
     logger.addHandler(handler)
-    logger.debug('Running notebook in debug mode.')
+    logger.setLevel(level)
 
-
-_setup_logging(logging.WARNING)
+    # If the bokeh logger is loaded before us, it messes with the config
+    # in bokeh/util/logconfig.py.  Set it up to also use the log file,
+    # and undo what it has done.
+    bokeh_logger = logging.getLogger('bokeh')
+    bokeh_logger.handlers.clear()
+    bokeh_logger.addHandler(handler)
 
 
 def setup_notebook(debug=False):
@@ -67,6 +74,7 @@ def setup_notebook(debug=False):
     output_notebook(INLINE, hide_banner=True)
     if debug:
         _setup_logging(logging.DEBUG)
+        logging.debug('Running notebook in debug mode.')
     else:
         _setup_logging(logging.WARNING)
 
